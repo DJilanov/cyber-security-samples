652fbd2e8ec7937859f66f0e23e6b6cdcd40e060
Dimitar Jilanov <dimitarjilanov@dimitars-macboo.local>
2022-04-17 13:51:44 +0300

    Small improvement over the example

--- demo5/server.js
+++ demo5/server.js
@@ -5,5 +5,5 @@ var path = require('path');
 // reading archives
 var zip = new AdmZip("./my_file.zip");
 var zipEntries = zip.getEntries(); // an array of ZipEntry records
-console.log('Entries: ', zipEntries.map((el) => el.name));
+console.log('Entries: ', zipEntries.map((el) => el.entryName));
 zip.extractAllTo(path.join(__dirname, '/folder/'), true);
